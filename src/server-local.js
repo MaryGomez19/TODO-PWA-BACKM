74e3900ebc9885610012f898c0cd7b95383187ef
MaryGomez19 <2123200430@soy.utj.edu.mx>
2026-02-24 14:45:57 -0600

    Corrección a port 5555

--- src/server-local.js
+++ src/server-local.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import app from "./app.js";
 
-const {PORT = 4000} = process.env;
+const {PORT = 5555} = process.env;
 
 app.listen(PORT, () => console.log(`Servidor ejecutandose en http://localhost:${PORT}`));
